8242ec862d189fe933c65f3ce213f197a2706085
Salatfreak <d.r.accounts@gmx.de>
2017-01-25 17:11:45 +0100

    Add location and scale handles

--- text.py
+++ text.py
@@ -1,7 +1,7 @@
 # ##### BEGIN GPL LICENSE BLOCK #####
 #
 #  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
+#  modify it under the terms of the GNUGeneral Public License
 #  as published by the Free Software Foundation; either version 2
 #  of the License, or (at your option) any later version.
 #
@@ -31,77 +31,57 @@ bl_info = {
 
 # Constants
 MAX_CHANNEL = 32
-DEFAULT_DIR = "//assets/title/"
+DEFAULT_DIR = "//renders/text"
 
 # Import modules
 import bpy
-import os
-import uuid
+import re
+from os import path
 
-### Helper functions ###
-########################
-
-# Get scene from ID
-def get_scene(scene_id):
-    # Find and return scene with matching ID
-    for scene in bpy.data.scenes:
-        if scene.text_props.id == scene_id:
-            return scene
-
-    # Return None if no scene matches
-    return None
-
-# Switch screen
-def switch_screen(context, screen_name):
-    for i in range(len(bpy.data.screens)):
-        # Break if found
-        if context.screen.name == screen_name: break
-
-        # Switch to next screen
-        bpy.ops.screen.screen_set(delta=1)
-
-### Object properties ###
+### Object Properties ###
 #########################
 
-# Text scene property group
-class SceneTextProps(bpy.types.PropertyGroup):
-    # Is text scene property
-    is_text_scene = bpy.props.BoolProperty(name="Is text Scene", default=False)
+# Location
+def get_location(self):
+    text_object = list(filter(
+        lambda o: o.type == 'FONT', self.scene.objects
+    ))[0]
+    return text_object.location.xy
+def set_location(self, location):
+    text_object = list(filter(
+        lambda o: o.type == 'FONT', self.scene.objects
+    ))[0]
+    text_object.location.xy = location
+text_location = bpy.props.FloatVectorProperty(
+    size=2, name="Location", get=get_location, set=set_location
+)
 
-    # Parent scene ID
-    parent_scene_id = bpy.props.StringProperty(name="Parent Scene ID")
-
-    # Get scenes
-    def get_screens(self, context):
-        return [(scr.name, scr.name, "") for scr in bpy.data.screens]
-
-    # Parent screen
-    parent_screen = bpy.props.EnumProperty(
-        name="Parent Screen", items=get_screens
-    )
-
-    # ID property
-    id = bpy.props.StringProperty(name="Unique ID", default="")
-
-    # Text edit screen
-    edit_screen = bpy.props.EnumProperty(name="Edit screen", items=get_screens)
-
-# Text strip property group
-class ImageStripTextProps(bpy.types.PropertyGroup):
-    # Is text strip property
-    is_text_strip = bpy.props.BoolProperty(name="Is text Strip", default=False)
-
-    # Scene ID
-    scene_id = bpy.props.StringProperty(name="Scene ID", default="")
+# Scale
+def get_scale(self):
+    text_object = list(filter(
+        lambda o: o.type == 'FONT', self.scene.objects
+    ))[0]
+    return text_object.scale.x
+def set_scale(self, scale):
+    text_object = list(filter(
+        lambda o: o.type == 'FONT', self.scene.objects
+    ))[0]
+    if text_object.scale.x == 0:
+        text_object.scale.yz = (scale, scale)
+    else:
+        text_object.scale.y = text_object.scale.y / text_object.scale.x * scale
+        text_object.scale.z = text_object.scale.z / text_object.scale.x * scale
+    text_object.scale.x = scale
+text_scale = bpy.props.FloatProperty(name="Scale", get=get_scale, set=set_scale)
 
 ### Effect operator ###
 ########################
 
-# Add rendered text operator
-class RenderedTextAddOperator(bpy.types.Operator):
+# Add text scene operator
+class TextSceneAddOperator(bpy.types.Operator):
     # Meta data
-    bl_idname="vse_text.rendered_text_effect_add"
-    bl_label="Add Rendered Text Effect"
+    bl_idname="sf_addons.text_scene_effect_add"
+    bl_label="Add Text Scene Effect"
     bl_options = {'REGISTER', 'UNDO'}
 
     # Properties
@@ -131,35 +111,12 @@ class RenderedTextAddOperator(bpy.types.Operator):
         # Store Sequencer Scene
         sequencer_scene = context.scene
 
-        # Add image strip
-        bpy.ops.sequencer.image_strip_add(
-            directory=DEFAULT_DIR,
-            frame_start=self.text_strip_start,
-            replace_sel=True,
-            use_placeholders=True
-        )
-        
-        # Get strip
-        text_strip = context.scene.sequence_editor.active_strip
-
-        # Set up strip
-        text_strip.name = "Text_"+ self.text.replace(" ", "_")\
-            .replace("\t", "_")[:16]
-        text_strip.frame_final_duration = self.text_strip_duration
-        text_strip.text_props.is_text_strip = True
-
         # Add new scene
-        text_scene = bpy.data.scenes.new(text_strip.name)
-        text_scene.text_props.is_text_scene = True
-        text_scene.text_props.id = uuid.uuid4().hex
-
-        # Link parent scene
-        if context.scene.text_props.id == "":
-            context.scene.text_props.id = uuid.uuid4().hex
-        text_scene.text_props.parent_scene_id = context.scene.text_props.id
-
-        # Link text scene
-        text_strip.text_props.scene_id = text_scene.text_props.id
+        text_scene = bpy.data.scenes.new(
+            "Text_"+ re.sub(
+                r'[^A-Za-z0-9_]', "", re.sub(r'\s', "", self.text)
+            )[:16]
+        )
 
         # Copy render settings
         text_scene.render.resolution_x = context.scene.render.resolution_x
@@ -171,22 +128,26 @@ class RenderedTextAddOperator(bpy.types.Operator):
         text_scene.render.use_overwrite = True
         text_scene.render.use_file_extension = False
         text_scene.render.image_settings.file_format = 'PNG'
-        text_scene.render.filepath = DEFAULT_DIR + text_scene.name[5:] +".png"
+        text_scene.render.filepath = path.join(
+            DEFAULT_DIR, text_scene.name[5:] +".png"
+        )
 
-        # Find first screen containing default
-        screen_found = False
-        for screen in bpy.data.screens:
-            if "default" in screen.name.lower():
-                text_scene.text_props.edit_screen = screen.name
-                screen_found = True
-                break
-
-        # Find first screen containing 3d else
-        if not screen_found:
+        # Set edit screen for scene tools addon
+        if hasattr(text_scene, 'sf_scene_props'):
+            # Find first screen containing default
+            screen_found = False
             for screen in bpy.data.screens:
-                if "3d" in screen.name.lower():
-                    text_scene.text_props.edit_screen = screen.name
+                if "default" in screen.name.lower():
+                    text_scene.sf_scene_props.edit_screen = screen.name
+                    screen_found = True
                     break
+
+            # Find first screen containing 3d else
+            if not screen_found:
+                for screen in bpy.data.screens:
+                    if "3d" in screen.name.lower():
+                        text_scene.sf_scene_props.edit_screen = screen.name
+                        break
 
         # Set up text scene
         context.screen.scene = text_scene
@@ -204,7 +165,7 @@ class RenderedTextAddOperator(bpy.types.Operator):
         bpy.ops.object.text_add()
         text_object = text_scene.objects.active
         text_object.data.offset_y = -0.35
-        text_object.data.align = 'CENTER'
+        text_object.data.align_x = 'CENTER'
         text_object.data.body = self.text
 
         # Set up material
@@ -222,122 +183,32 @@ class RenderedTextAddOperator(bpy.types.Operator):
         # Switch back to sequencer scene
         context.screen.scene = sequencer_scene
 
+        # Add text scene strip
+        bpy.ops.sequencer.scene_strip_add(
+            scene=text_scene.name,
+            frame_start=self.text_strip_start,
+            replace_sel=True
+        )
+
+        # Get strip
+        text_strip = context.scene.sequence_editor.active_strip
+
+        # Set up strip
+        text_strip.name = text_scene.name
+        text_strip.frame_final_duration = self.text_strip_duration
+
         return {'FINISHED'}
 
-# Rendered text button
-def rendered_text_button(self, context):
+# Text scene button
+def text_scene_button(self, context):
     self.layout.operator(
-        RenderedTextAddOperator.bl_idname,
-        text="Rendered Text",
+        TextSceneAddOperator.bl_idname,
+        text="Text Scene",
         icon='PLUGIN'
     )
 
 ### Rendered text strip panel ###
 #################################
-
-# Render text operator
-class RenderTextOperator(bpy.types.Operator):
-    # Meta data
-    bl_idname = "vse_text.render_text"
-    bl_label = "Render Text"
-    bl_description = "Render Text Scene"
-
-    # Show only for text strips
-    @classmethod
-    def poll(cls, context):
-        if context.scene.sequence_editor is None: return False
-        strip = context.scene.sequence_editor.active_strip
-        return (strip is not None and strip.type == 'IMAGE' and \
-            strip.text_props.is_text_strip and \
-            get_scene(strip.text_props.scene_id) is not None)
-
-    # Render text
-    def invoke(self, context, event):
-        strip = context.scene.sequence_editor.active_strip
-        scene = get_scene(strip.text_props.scene_id)
-
-        # Render text
-        bpy.ops.render.render(write_still=True, scene=scene.name)
-
-        # Update image
-        while len(strip.elements) > 1:
-            strip.elements.pop(0)
-        strip.elements.append(os.path.split(scene.render.filepath)[-1])
-        strip.elements.pop(0)
-        
-        return {'FINISHED'}
-
-# Switch to text editing operator
-class SwitchToTextEditingOperator(bpy.types.Operator):
-    # Meta data
-    bl_idname = "vse_text.switch_to_text_editing"
-    bl_label = "Switch to Text Editing"
-    bl_description = "Switch to text scene editing"
-
-    # Show only for text strips
-    @classmethod
-    def poll(self, context):
-        if context.scene.sequence_editor is None: return False
-        strip = context.scene.sequence_editor.active_strip
-        return (strip is not None and strip.type == 'IMAGE' and \
-            strip.text_props.is_text_strip and \
-            get_scene(strip.text_props.scene_id) is not None)
-
-    # Switch scene
-    def invoke(self, context, event):
-        strip = context.scene.sequence_editor.active_strip
-        text_scene = get_scene(strip.text_props.scene_id)
-
-        # Set scene properties
-        text_scene.text_props.parent_screen = context.screen.name
-
-        # Get text editing screen
-        switch_screen(context, text_scene.text_props.edit_screen)
-
-        # Switch to scene
-        context.screen.scene = text_scene
-
-        return {'FINISHED'}
-
-# Remove text strip operator
-class RemoveTextStripOperator(bpy.types.Operator):
-    # Meta data
-    bl_idname = "vse_text.remove_text_strip"
-    bl_label = "Remove text strip"
-    bl_description = "Remove text scene and sequencer strip"
-
-    # Show only for text Strips
-    @classmethod
-    def poll(cls, context):
-        if context.scene.sequence_editor is None: return False
-        strip = context.scene.sequence_editor.active_strip
-        return (strip is not None and strip.type == 'IMAGE' and \
-            strip.text_props.is_text_strip and \
-            get_scene(strip.text_props.scene_id) is not None)
-
-    # Show confirmation dialog
-    def invoke(self, context, event):
-        wm = context.window_manager
-        return wm.invoke_confirm(self, event)
-
-    # Remove scene and strip
-    def execute(self, context):
-        strip = context.scene.sequence_editor.active_strip
-        scene = get_scene(strip.text_props.scene_id)
-
-        # Remove scene
-        bpy.data.scenes.remove(scene)
-
-        # Remove strip (taking groups into account)
-        selected = context.selected_sequences
-        for seq in selected:
-            seq.select = False
-        strip.select = True
-        bpy.ops.sequencer.delete()
-        for seq in selected:
-            seq.select = True
-
-        return {'FINISHED'}
 
 # Text strip panel
 class TextStripPanel(bpy.types.Panel):
@@ -351,107 +222,40 @@ class TextStripPanel(bpy.types.Panel):
     def poll(self, context):
         if context.scene.sequence_editor is None: return False
         strip = context.scene.sequence_editor.active_strip
-        return (strip is not None and strip.type =='IMAGE' and \
-            strip.text_props.is_text_strip and \
-            get_scene(strip.text_props.scene_id) is not None)
+        return (
+            strip is not None \
+            and strip.type == 'SCENE' \
+            and strip.scene is not None \
+            and sum(o.type == 'FONT' for o in strip.scene.objects) > 0
+        )
 
     # Draw panel
     def draw(self, context):
         strip = context.scene.sequence_editor.active_strip
-        scene = get_scene(strip.text_props.scene_id)
 
         text_objects = list(filter(
-            lambda o: o.type == 'FONT', scene.objects
+            lambda o: o.type == 'FONT', strip.scene.objects
         ))
 
         # Text
-        if len(text_objects) == 1:
-            text_object = text_objects[0]
-            self.layout.prop(text_object.data, "body", text="")
-            
-            # Color align row
-            color_align_row = self.layout.row()
-            if len(text_object.material_slots) == 1 and \
-                text_object.material_slots[0].material is not None:
-                material = text_object.material_slots[0].material
-            
+        text_object = text_objects[0]
+        self.layout.prop(text_object.data, "body", text="")
+
+        # Positioning
+        self.layout.prop(strip, "sf_text_location", text="Loc")
+        self.layout.prop(strip, "sf_text_scale")
+
+        # Material
+        if len(text_object.material_slots) == 1 and \
+            text_object.material_slots[0].material is not None:
+            material = text_object.material_slots[0].material
+
             # Color row
             color_row = self.layout.row()
             color_row.prop(material, "diffuse_color", text="")
-            if scene.world is not None:
-                color_row.prop(scene.render, "alpha_mode", text="")
-                color_row.prop(scene.world, "horizon_color", text="")
-
-        # Rendering
-        self.layout.prop(scene.render, "filepath", text="")
-        self.layout.operator(
-            RenderTextOperator.bl_idname, text="Render", icon='RENDER_STILL'
-        )
-        
-        # Edit
-        self.layout.separator()
-        self.layout.operator(
-            SwitchToTextEditingOperator.bl_idname, text="Edit Text",
-            icon='TEXT'
-        )
-        self.layout.prop(
-            scene.text_props, 'edit_screen', text="", icon='SPLITSCREEN'
-        )
-        
-        # Removal
-        self.layout.operator(
-            RemoveTextStripOperator.bl_idname, text="Remove", icon='X'
-        )
-
-### Text scene panel ###
-#############################
-
-# Switch to sequence editor operator
-class SwitchToSequenceEditorOperator(bpy.types.Operator):
-    # Meta data
-    bl_idname = "vse_text.switch_to_sequence_editor"
-    bl_label = "Back to Sequencer"
-    bl_description = "Switch back to the Sequence Editor"
-
-    # Show only for text scenes
-    @classmethod
-    def poll(self, context):
-        return context.scene.text_props.is_text_scene
-
-    # Switch scene
-    def invoke(self, context, event):
-        # Get text properties
-        text_props = context.scene.text_props
-
-        # Switch screen
-        switch_screen(context, text_props.parent_screen)
-
-        # Switch to scene
-        scene = get_scene(text_props.parent_scene_id)
-        if scene is not None:
-            context.screen.scene = scene
-        else:
-            self.report({'ERROR'}, "Sequence editing scene missing")
-
-        return {'FINISHED'}
-
-# Switch to sequence editor Panel
-class TextScenePanel(bpy.types.Panel):
-    # Meta data
-    bl_label = "Text Strip"
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "UI"
-
-    # Show only for text scenes
-    @classmethod
-    def poll(self, context):
-        return context.scene.text_props.is_text_scene
-
-    # Draw panel
-    def draw(self, context):
-        self.layout.operator(
-            SwitchToSequenceEditorOperator.bl_idname, icon='SEQ_SEQUENCER'
-        )
+            if strip.scene.world is not None:
+                color_row.prop(strip.scene.render, "alpha_mode", text="")
+                color_row.prop(strip.scene.world, "horizon_color", text="")
 
 ### Module registration ###
 ###########################AA
@@ -461,32 +265,24 @@ def register():
     # Register module
     bpy.utils.register_module(__name__)
 
-    # Register image strip properties
-    bpy.types.ImageSequence.text_props = bpy.props.PointerProperty(
-        type=ImageStripTextProps
-    )
-
-    # Register scene properties
-    bpy.types.Scene.text_props = bpy.props.PointerProperty(
-        type=SceneTextProps
-    )
+    # Register strip properties
+    bpy.types.SceneSequence.sf_text_location = text_location
+    bpy.types.SceneSequence.sf_text_scale = text_scale
 
     # Add button
-    bpy.types.SEQUENCER_MT_add_effect.append(rendered_text_button)
+    bpy.types.SEQUENCER_MT_add_effect.append(text_scene_button)
 
 # Unregister module
 def unregister():
     # Unregister module
     bpy.utils.unregister_module(__name__)
 
-    # Unregister text properties
-    del bpy.types.ImageSequence.text_props
-
-    # Unregister scene properties
-    del bpy.types.Scene.text_props
+    # Unregister strip properties
+    del bpy.types.SceneSequence.sf_text_location
+    del bpy.types.SceneSequence.sf_text_scale
 
     # Remove button
-    bpy.types.SEQUENCER_MT_add_effect.remove(rendered_text_button)
+    bpy.types.SEQUENCER_MT_add_effect.remove(text_scene_button)
 
 # Register if executed as script
 if __name__ == '__main__':
